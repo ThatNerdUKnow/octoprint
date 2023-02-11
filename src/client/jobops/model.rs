4cccdeda4ff1260e78dada3255e2098463334869
Brandon Piña <brandonpi265@gmail.com>
2023-02-11 14:03:14 -0600

    add progress information struct

--- src/client/jobops/model.rs
+++ src/client/jobops/model.rs
@@ -11,3 +11,31 @@ pub struct JobInfo {
     pub last_print_time: Option<f64>,
     pub filament: Option<Filament>
 }
+
+#[derive(Serialize,Deserialize)]
+pub struct ProgressInfo{
+    completion: f64,
+    filepos: usize,
+    #[serde(rename="printTime")]
+    print_time: usize,
+    #[serde(rename="printTimeLeft")]
+    print_time_left: usize,
+    #[serde(rename="printTImeLeftOrigin")]
+    print_time_left_origin: PrintTImeLeftOrigin
+}
+
+#[derive(Serialize,Deserialize)]
+pub enum PrintTImeLeftOrigin{
+    #[serde(rename="linear")]
+    Linear,
+    #[serde(rename="analysis")]
+    Analysis,
+    #[serde(rename="estimate")]
+    Estimate,
+    #[serde(rename="average")]
+    Average,
+    #[serde(rename="mixed-analysis")]
+    MixedAnalysis,
+    #[serde(rename="mixed-average")]
+    MixedAverage
+}
