b556cf05aa263c0134dab5806877fc40958a52ed
Brandon Piña <brandonpi265@gmail.com>
2023-02-11 13:31:50 -0600

    add inline docs to fileinfo

--- src/client/fileops/model.rs
+++ src/client/fileops/model.rs
@@ -1,15 +1,34 @@
-use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::path::PathBuf;
 use url_serde;
 use url_serde::SerdeUrl as Url;
 
 #[derive(Serialize, Deserialize)]
 pub struct FileInfo {
+    /// The name of the file without path. E.G. "file.gco"
+    /// for a file "file.gco" located anywhere in the file system.
+    /// Currently this will always fit into ASCII.
     pub name: String,
+    /// The name of the file without the path, this time potentially
+    /// with non-ASCII unicode characters. E.G.
+    /// “a turtle 🐢.gco” for a file “a_turtle_turtle.gco”
+    /// located anywhere in the file system.
     pub display: String,
-    pub path: String,
+    /// The path to the file within the location. E.g.
+    /// "folder/subfolder/file.gco" for a file
+    /// "file.gco" located within "folder" and "subfolder"
+    /// relative to the root of the location.
+    /// Currently this will always fit nto ASCII
+    pub path: PathBuf,
+    /// Type of the file. `model` or `machinecode`. Or `folder`
+    /// if it's a folder, in which case the children node will be populated
     #[serde(rename = "type")]
     pub model_type: ModelType,
+    /// Path to type of file in extension tree. E.g.
+    /// `["model","stl"]` for `.stl`
+    /// files or `["machinecode","gcode"]` for 
+    /// `.gcode` files. `["folder"]` for folders
     #[serde(rename = "typePath")]
     pub type_path: Vec<String>,
 }
@@ -35,7 +54,7 @@ pub struct File {
     pub date: Option<usize>,
     pub origin: FileOrigin,
     pub refs: Option<References>,
-    #[serde(rename="gcodeAnalysis")]
+    #[serde(rename = "gcodeAnalysis")]
     pub gcode_analysis: Option<GCodeAnalysis>,
     pub prints: Option<PrintHistory>,
     pub statistics: Option<PrintStatistics>,
@@ -66,15 +85,15 @@ pub struct PrintHistory {
 
 #[derive(Serialize, Deserialize)]
 pub struct PrintStatistics {
-    #[serde(rename="averagePrintTime")]
+    #[serde(rename = "averagePrintTime")]
     pub average_print_time: HashMap<String, usize>,
-    #[serde(rename="lastPrintTime")]
+    #[serde(rename = "lastPrintTime")]
     pub last_print_time: HashMap<String, usize>,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct GCodeAnalysis {
-    #[serde(rename="estimatedPrintTime")]
+    #[serde(rename = "estimatedPrintTime")]
     pub estimated_print_time: Option<f64>,
     pub filament: Option<Filament>,
 }
@@ -91,9 +110,8 @@ pub struct Dimensions {
     pub width: f64,
 }
 #[derive(Serialize, Deserialize)]
-#[serde(rename_all="camelCase")]
+#[serde(rename_all = "camelCase")]
 pub struct PrintingArea {
-
     pub max_x: f64,
     pub max_y: f64,
     pub max_z: f64,
