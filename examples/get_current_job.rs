0ea7cc786353960fa71fa135f888378c0aec580d
Brandon Piña <32885853+ThatNerdUKnow@users.noreply.github.com>
2023-02-11 19:40:18 -0600

    Update get_current_job.rs

--- examples/get_current_job.rs
+++ examples/get_current_job.rs
@@ -4,10 +4,14 @@ use octoprint::client::{OctoClient, OctoClientBuilder};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    // Get environment variables
     let url: String = env::var("OCTOPRINT_URL")?;
     let key: String = env::var("OCTOPRINT_API_KEY")?;
+    
+    // Instantiate a client
     let octo = OctoClient::new(&url)?.use_api_key(&key)?.build()?;
 
+    // execute a query against the octoprint api to get the current job
     let currentJob = octo.current_job().await?;
     println!("{currentJob:?}");
     Ok(())
