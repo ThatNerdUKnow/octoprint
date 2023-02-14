5997d69cc63c54dfb5133613f7115c4e4d06f85d
Brandon Piña <brandonpi265@gmail.com>
2023-02-13 22:43:15 -0600

    This one should probably work

--- src/client/job/operations.rs
+++ src/client/job/operations.rs
@@ -26,7 +26,7 @@ impl OctoClient {
         return Ok(response);
     }
 }
-
+/*
 #[derive(Serialize, Deserialize, Debug)]
 pub struct JobInformationResponse {
     /// Information regarding the target of the current print job
@@ -40,4 +40,28 @@ pub struct JobInformationResponse {
     state: String,
     /// Any error message for the job or connection. Only set if there has been an error
     error: Option<String>,
+}*/
+
+#[derive(Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum JobInformationResponse {
+    Online {
+        /// Information regarding the target of the current print job
+        job: JobInfo,
+        /// Information regarding the progress of the current print job
+        progress: ProgressInfo,
+        /// A textual representation of the current state of the job
+        /// or connection. e.g. "Operational", "Printing", "Pausing",
+        /// "Paused", "Cancelling", "Error", "Offline", "Offline after error",
+        /// "Opening serial connection" ... - please note that this list is not exhaustive!
+        state: String,
+    },
+    Offline {
+        /// Same as above state, but we'll assume if other props don't deserialize correctly, that the printer is offline
+        state: String,
+    },
+    Error {
+        /// Any error message for the job or connection. Only set if there has been an error
+        error: Option<String>,
+    },
 }
