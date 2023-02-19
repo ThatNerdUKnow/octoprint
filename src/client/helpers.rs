3218b01cc9f12792bdc790397b7c712c328b1a2c
Brandon Piña <brandonpi265@gmail.com>
2023-02-19 12:56:55 -0600

    post helper

--- src/client/helpers.rs
+++ src/client/helpers.rs
@@ -9,9 +9,29 @@ impl OctoClient {
     pub(super) fn get(&self, path: &str) -> Result<RequestBuilder, OctoClientError> {
         let url = self
             .append_path_to_base_url(path)
-            .change_context(OctoClientError::BuildRequest)?;
+            .change_context(OctoClientError::BuildRequest)
+            .attach_printable_lazy(|| self.base_url.to_owned())
+            .attach_printable_lazy(|| path.to_owned())?;
         //let builder = self.client.get(url);
         let builder = self.add_auth(self.client.get(url));
+
+        Ok(builder)
+    }
+
+    pub(super) fn post(
+        &self,
+        path: &str,
+        payload: &str,
+    ) -> Result<RequestBuilder, OctoClientError> {
+        let url = self
+            .append_path_to_base_url(path)
+            .change_context(OctoClientError::BuildRequest)
+            .attach_printable_lazy(|| self.base_url.to_owned())
+            .attach_printable_lazy(|| path.to_owned())?;
+
+        let mut builder = self.client.post(url);
+        builder = self.add_auth(builder);
+        builder = builder.body(payload.to_owned());
 
         Ok(builder)
     }
